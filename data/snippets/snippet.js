d97686ef077937514c740042c74e584b635965b5
Arthur Wallendorff <howdy@wallenart.dev>
2024-08-15 20:47:47 +0200

    chore: add better typing examples

--- data/snippets/snippet.js
+++ data/snippets/snippet.js
@@ -1,49 +1,55 @@
 function colorizeCode() {
-    const element = document.getElementsByTagName('code')[0];
+  const element = document.getElementsByTagName("code")[0];
 
-    if (element == null) {
-        return;
-    }
+  if (element == null) {
+    return;
+  }
 
-    const text = element.innerText;
-    const language = element.classList[0];
+  const text = element.innerText;
+  const language = element.classList[0];
 
-    const keywords = {
-        js: javascriptKeywords,
-        ts: typescriptKeywords,
-        html: htmlKeywords,
-        css: cssKeywords,
-        go: goKeywords,
-        c: cKeywords,
-        gql: graphqlKeywords,
-        sql: sqlKeywords,
-        jsx: reactKeywords,
-        jest: testKeywords,
-        sh: bashKeywords,
-        yml: yamlKeywords,
-        ino: arduinoKeywords,
-    }[language];
+  const keywords = {
+    js: javascriptKeywords,
+    ts: typescriptKeywords,
+    html: htmlKeywords,
+    css: cssKeywords,
+    go: goKeywords,
+    c: cKeywords,
+    gql: graphqlKeywords,
+    sql: sqlKeywords,
+    jsx: reactKeywords,
+    jest: testKeywords,
+    sh: bashKeywords,
+    yml: yamlKeywords,
+    ino: arduinoKeywords,
+  }[language];
 
-    if (keywords == null) {
-        return;
-    }
+  if (keywords == null) {
+    return;
+  }
 
-    let coloredText = text
+  let coloredText = text;
 
-    keywords.forEach((subKeywords, i) => {
-        const regex = new RegExp(`\\b(${subKeywords.join('|')})\\b`, 'g');
-        coloredText = coloredText.replaceAll(regex, (match) =>
-            `<span class="${COLORS[i]}">${match}</span>`
-        );
-    })
+  keywords.forEach(
+    (/** @type {[]string} **/ subKeywords, /** @type {number} **/ i) => {
+      const regex = new RegExp(`\\b(${subKeywords.join("|")})\\b`, "g");
+      coloredText = coloredText.replaceAll(
+        regex,
+        (match) => `<span class="${COLORS[i]}">${match}</span>`,
+      );
+    },
+  );
 
-    element.innerHTML = coloredText;
+  element.innerHTML = coloredText;
 }
 
+/**
+ * @param {string} snippet
+ * @returns {Promise<string>}
+ */
 async function getCodeSnippet(snippet) {
-    const response = await fetch(`../data/snippets/snippet.${snippet}`)
-    const text = await response.text();
+  const response = await fetch(`../data/snippets/snippet.${snippet}`);
+  const text = await response.text();
 
-    return text;
+  return text;
 }
-
